68e268d0929d071026588aca86d3f64fcc8097b0
Harish Babu <harish_babu@outlook.com>
2016-08-15 22:07:00 +0100

    initial aurelia-cli new project commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+
+export class App {
+
+  constructor() {
+  }
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    this.router = router;
+
+    config.title = 'Bizhub';
+    config.options.pushState = true;
+    config.options.root = '/';
+    config.map([
+      { 
+        route: ['', 'home'], 
+        name: 'home', 
+        moduleId: 'home/index'
+      },
+      {
+        route: 'timesheets', 
+        name: 'timesheets', 
+        moduleId: 'timesheets/index',
+        title: 'Timesheets',
+        nav: true 
+      }
+    ]);
+
+    config.mapUnknownRoutes('not-found');
+  }
+}
