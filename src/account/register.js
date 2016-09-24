d3ea2d12218e555f535fbc6fb823d40d160aed8a
Harish Babu <harish_babu@outlook.com>
2016-09-24 19:48:06 +0100

    can now successfully register a new user

--- src/account/register.js
+++ src/account/register.js
@@ -1,3 +1,29 @@
+import {Router} from 'aurelia-router';
+import {AccountService} from '../services/account-service'
+
 export class Register {
-    
+
+    static inject() {
+      return [
+        AccountService,
+        Router
+      ]
+    };
+
+    constructor(accountService, router) {
+        this.accountService = accountService;
+        this.router = router;
+    };
+
+    register() {
+        var self = this;
+        this.accountService.create({
+            email: this.email,
+            password: this.password,
+            givenName: this.firstName,
+            surname: this.lastName
+        }).then(function(){
+            self.router.navigateToRoute('login');
+        });
+    }
 }
