88d8a7f1f2f61dd183ca680d3aeb8bc7d636c996
Harish Babu <harish_babu@outlook.com>
2017-06-10 00:51:53 +0100

    implement deleting of an organization

--- src/organizations/detail.js
+++ src/organizations/detail.js
@@ -1,23 +1,27 @@
 import {inject, Factory} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
 import {ApiService} from '../services/api-service';
 
-@inject(Factory.of(ApiService))
+@inject(Router, Factory.of(ApiService))
 export class Detail {
 
-  constructor(apiService) {
-    this.organizationService = apiService('organization');
-    this.organization = {};
-  }
+    constructor(router, apiService) {
+        this.router = router;
+        this.organizationService = apiService('organization');
+        this.organization = {};
+    }
 
-  activate(params) {
-    return this.organizationService.getById(params.id)
-    .then((org) => {
-      this.organization = org;
-    });
-  }
+    activate(params) {
+        return this.organizationService.getById(params.id)
+        .then((org) => {
+            this.organization = org;
+        });
+    }
 
-  delete() {
-      this.organizationService.delete(this.organization.id)
-      .then(() => this.router.navigateToRoute('organizations'));
-  }
+    delete() {
+        this.organizationService.delete(this.organization.id)
+        .then(() => 
+            this.router.navigateToRoute('organizations')
+        );
+    }
 }
