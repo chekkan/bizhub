86a7165146939f0e61ec5acc1f63cfcb4cbeac05
Harish Babu <harish_babu@outlook.com>
2017-06-11 02:41:13 +0100

    cannot submit invalid create-form

--- src/resources/components/create-form/index.js
+++ src/resources/components/create-form/index.js
@@ -1,15 +1,16 @@
 import { inject, bindable, bindingMode } from "aurelia-framework"
 import { ValidationRules, ValidationControllerFactory } from "aurelia-validation"
 
-@inject(ValidationControllerFactory)
+@inject(ValidationControllerFactory, Element)
 export class CreateFormCustomElement {
   @bindable schema;
   @bindable({ defaultBindingMode: bindingMode.twoWay })
     model;
 
-    constructor(controllerFactory) {
+    constructor(controllerFactory, element) {
         this.rules = []
         this.controller = controllerFactory.createForCurrentScope()
+        this.element = element
     }
 
     bind() {
@@ -33,5 +34,17 @@ export class CreateFormCustomElement {
             },
         )
         .reduce((prev, curr) => prev.concat(curr))
+    }
+
+    validate() {
+        this.controller.validate({ object: this.model, rules: this.rules })
+        .then((result) => {
+            debugger;
+            if (result.valid) {
+                this.element.dispatchEvent(new Event("submit", {
+                    bubbles: true,
+                }))
+            }
+        })
     }
 }
