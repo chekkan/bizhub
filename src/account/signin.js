7eb5d273bf43a1c48946a1e2290a7a131a1086c5
Harish Babu <harish_babu@outlook.com>
2017-06-28 21:37:20 +0100

    add id_token to response type

--- src/account/signin.js
+++ src/account/signin.js
@@ -5,7 +5,7 @@ import { AuthService } from "../services/auth-service"
 export class SignInViewModel {
     constructor(authService) {
         let href = authService.config.authEndpoint
-        href += `?response_type=token&client_id=${authService.config.clientId}`
+        href += `?response_type=id_token token&client_id=${authService.config.clientId}`
         href += `&connection=${authService.config.connectionName}`
         href += `&redirect_uri=${authService.config.redirectUri}`
         window.location.replace(href)
