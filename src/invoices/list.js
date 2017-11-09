6e77e3b793605c6a51febc9d5cef544e55ea3b33
Harish Babu <harish_babu@outlook.com>
2017-11-09 23:09:52 +0000

    list organization name and office address when displaying invoice list

--- src/invoices/list.js
+++ src/invoices/list.js
@@ -10,6 +10,8 @@ export class InvoicesListViewModel extends ListViewModel {
     constructor(actStrategy, apiService) {
         super(actStrategy, apiService("invoice"))
         this.timeEntryService = apiService("time-entry")
+        this.orgService = apiService("organization")
+        this.officeService = apiService("office")
         this.logger = LogManager.getLogger("InvoicesListViewModel")
     }
 
@@ -19,20 +21,44 @@ export class InvoicesListViewModel extends ListViewModel {
             const timeEntryIds = invoices.map(inv => inv.timeEntries.map(te => te.id))
                 .reduce((acc, val) => [...acc, ...val])
                 .filter((v, i, a) => a.indexOf(v) === i)
-            return this.timeEntryService.getAll(timeEntryIds.length, 0, {
-                id: timeEntryIds.map(te => te),
+
+            const organizationIds = invoices.map(inv => inv.recipient.organization.id)
+                .filter((v, i, a) => a.indexOf(v) === i)
+
+            const officeIds = invoices.map(inv => inv.recipient.office.id)
+                .filter((v, i, a) => a.indexOf(v) === i)
+
+            const tePromise = this.timeEntryService.getAll(timeEntryIds.length, 0, {
+                id: timeEntryIds,
+            }).then(teRes => teRes.content)
+
+            const orgPromise = this.orgService.getAll(organizationIds.length, 0, {
+                id: organizationIds,
+            }).then(orgRes => orgRes.content)
+
+            const officePromise = this.officeService.getAll(officeIds.length, 0, {
+                id: officeIds,
+            }).then(officeRes => officeRes.content)
+
+            return Promise.all([tePromise, orgPromise, officePromise])
+            .then((responses) => {
+                const timeEntries = responses[0]
+                const organizations = responses[1]
+                const offices = responses[2]
+                return invoices.map(inv => Object.assign({}, inv, {
+                    timeEntries: inv.timeEntries
+                        .map(te => timeEntries.filter(entry => entry.id === te.id)[0]),
+                    recipient: {
+                        organization: organizations
+                            .filter(org => org.id === inv.recipient.organization.id)[0],
+                        office: offices.filter(o => o.id === inv.recipient.office.id)[0],
+                    },
+                }))
             })
-            .then(teRes => teRes.content)
-            .then(timeEntries => (invoices.map(inv => Object.assign({}, inv, {
-                timeEntries: inv.timeEntries
-                    .map(te => timeEntries.filter(entry => entry.id === te.id)[0]),
-            }))))
         })
         .then((invoices) => {
             this.logger.debug(invoices)
-            this.invoices = invoices.map(inv => ({
-                id: inv.id,
-                date: inv.date,
+            return invoices.map(inv => Object.assign({}, inv, {
                 totalAmount: inv.timeEntries.reduce((acc, val) => {
                     const msInHours = (1000 * 60 * 60)
                     const breakInHrs = val.break / 60
@@ -41,7 +67,9 @@ export class InvoicesListViewModel extends ListViewModel {
                     return (acc + (totalHours * val.ratePerHour))
                 }, 0),
             }))
-            this.logger.debug(this.invoices)
+        })
+        .then((invoices) => {
+            this.invoices = invoices
         })
     }
 }
