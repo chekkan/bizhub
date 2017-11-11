54464b9ee49285d6c84698469e558ebf706edda2
Harish Babu <harish_babu@outlook.com>
2017-11-11 15:19:48 +0000

    :lipstick: invoice details view now shows invoice item description

--- src/invoices/detail.js
+++ src/invoices/detail.js
@@ -18,33 +18,56 @@ export class Detail {
     activate(params) {
         return this.invoiceService.getById(params.id)
         .then((invoice) => {
-            const tePromise = this.timeEntryService.getAll(invoice.timeEntries.length, 0, {
+            this.invoice = invoice
+            return this.timeEntryService.getAll(invoice.timeEntries.length, 0, {
                 id: invoice.timeEntries.map(te => te.id),
-            }).then(response => response.content)
-            const orgPromise = this.orgService.getById(invoice.recipient.organization.id)
-            const officePromise = this.officeService.getById(invoice.recipient.office.id)
-            
-            return Promise.all([tePromise, orgPromise, officePromise])
-            .then((responses) => {
-                const timeEntries = responses[0]
-                const organization = responses[1]
-                const office = responses[2]
+            })
+        })
+        .then(response => response.content)
+        .then((timeEntries) => {
+            this.invoice.timeEntries = timeEntries
 
-                timeEntries.map((te) => {
-                    const msInHours = 1000 * 60 * 60
-                    const breakInHrs = te.break / 60
-                    const workTimeInMs = new Date(te.end) - new Date(te.start)
-                    const totalHours = (workTimeInMs / msInHours) - (breakInHrs)
-                    return {
-                        start: te.start,
-                        end: te.end,
-                        totalAmount: (totalHours * te.ratePerHour),
-                    }
-                })
-                this.invoice = invoice
-                this.invoice.timeEntries = timeEntries
-                this.invoice.recipient.organization = organization
-                this.invoice.recipient.office = office
+            const organizationIds = timeEntries.map(te => te.organization.id)
+                .concat(this.invoice.recipient.organization.id)
+                .filter((v, i, a) => a.indexOf(v) === i)
+
+            const officeIds = timeEntries.map(te => te.office.id)
+                .concat(this.invoice.recipient.office.id)
+                .filter((v, i, a) => a.indexOf(v) === i)
+
+            const orgsPromise = this.orgService.getAll(organizationIds.length, 0, {
+                id: organizationIds,
+            }).then(orgRes => orgRes.content)
+
+            const officesPromise = this.officeService.getAll(officeIds.length, 0, {
+                id: officeIds,
+            }).then(orgRes => orgRes.content)
+
+            return Promise.all([orgsPromise, officesPromise])
+        })
+        .then((timeEntryChildResponses) => {
+            const organizations = timeEntryChildResponses[0]
+            const offices = timeEntryChildResponses[1]
+
+            this.invoice.recipient.organization = organizations
+                .filter(org => org.id === this.invoice.recipient.organization.id)[0]
+            this.invoice.recipient.office = offices
+                .filter(off => off.id === this.invoice.recipient.office.id)[0]
+
+            this.invoice.timeEntries = this.invoice.timeEntries.map((te) => {
+                const msInHours = 1000 * 60 * 60
+                const breakInHrs = te.break / 60
+                const workTimeInMs = new Date(te.end) - new Date(te.start)
+                const totalHours = (workTimeInMs / msInHours) - (breakInHrs)
+                return {
+                    start: te.start,
+                    end: te.end,
+                    ratePerHour: te.ratePerHour,
+                    billableHours: totalHours,
+                    totalAmount: (totalHours * te.ratePerHour),
+                    organization: organizations.filter(org => org.id === te.organization.id)[0],
+                    office: offices.filter(off => off.id === te.office.id)[0],
+                }
             })
         })
     }
