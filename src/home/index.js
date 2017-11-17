8a56eacfc50744c3e3cd7a4b9193a6b39b7a2f96
Harish Babu <harish_babu@outlook.com>
2017-11-17 00:10:14 +0000

    :lipstick: home page for visitor added with sign up form and welcome message

--- src/home/index.js
+++ src/home/index.js
@@ -1,3 +1,34 @@
+import { PLATFORM } from "aurelia-pal"
+import { inject } from "aurelia-framework"
+import { AuthService } from "../services/auth-service"
+
+@inject(AuthService)
 export class Index {
 
+    authService = null
+
+    constructor(authService) {
+        this.authService = authService
+    }
+
+    configureRouter(config, router) {
+        const navStrat = (instruction) => {
+            const isLoggedIn = this.authService.isAuthenticated
+
+            const moduleId = isLoggedIn ? PLATFORM.moduleName("home/authorized")
+                : PLATFORM.moduleName("home/visitor")
+            Object.assign(instruction.config, {
+                moduleId,
+                href: instruction.fragment,
+            })
+        }
+        config.map([
+            {
+                route: "",
+                name: "home",
+                navigationStrategy: navStrat,
+            },
+        ])
+        this.router = router
+    }
 }
