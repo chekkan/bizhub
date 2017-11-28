77b031649930a7f46bd3f7faefa3e4f5dc999e91
Harish Babu <harish_babu@outlook.com>
2017-11-28 01:48:18 +0000

    updated webpack config with sass and css

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,33 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const CopyWebpackPlugin = require("copy-webpack-plugin")
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
-const { AureliaPlugin } = require("aurelia-webpack-plugin")
-const { optimize: { CommonsChunkPlugin }, ProvidePlugin } = require("webpack")
-
-const configSass = require("./webpack/config-sass")
-const configCss = require("./webpack/config-css")
+const autoprefixer = require("autoprefixer")
+const project = require("./aurelia_project/aurelia.json")
+const { AureliaPlugin, ModuleDependenciesPlugin } = require("aurelia-webpack-plugin")
+const { optimize: { CommonsChunkPlugin, UglifyJsPlugin }, ProvidePlugin } = require("webpack")
 
 // config helpers:
 const ensureArray = config =>
-  (config && (Array.isArray(config) ? config : [config])) || []
+    (config && (Array.isArray(config) ? config : [config])) || []
 const when = (condition, config, negativeConfig) =>
-  (condition ? ensureArray(config) : ensureArray(negativeConfig))
+    (condition ? ensureArray(config) : ensureArray(negativeConfig))
 
 // primary config:
 const title = "Busy Nest"
-const outDir = path.resolve(__dirname, "dist")
+const outDir = path.resolve(__dirname, project.platform.output)
 const srcDir = path.resolve(__dirname, "src")
 const nodeModulesDir = path.resolve(__dirname, "node_modules")
 const baseUrl = "/"
+
+const cssRules = [
+    { loader: "css-loader" },
+    {
+        loader: "postcss-loader",
+        options: {
+            plugins: () => [autoprefixer({ browsers: ["last 2 versions"] })],
+        },
+    },
+]
 
 module.exports = ({ production, server, extractCss, coverage } = {}) => ({
     resolve: {
@@ -27,50 +36,77 @@ module.exports = ({ production, server, extractCss, coverage } = {}) => ({
         modules: [srcDir, "node_modules"],
     },
     entry: {
-        app: ["aurelia-bootstrapper", `${srcDir}/scss/main.scss`],
+        app: ["aurelia-bootstrapper"],
         vender: ["bluebird", "jquery", "popper.js", "bootstrap"],
     },
     output: {
         path: outDir,
         publicPath: baseUrl,
         filename: production
-      ? "[name].[chunkhash].bundle.js"
-      : "[name].[hash].bundle.js",
+            ? "[name].[chunkhash].bundle.js"
+            : "[name].[hash].bundle.js",
         sourceMapFilename: production
-      ? "[name].[chunkhash].bundle.map"
-      : "[name].[hash].bundle.map",
+            ? "[name].[chunkhash].bundle.map"
+            : "[name].[hash].bundle.map",
         chunkFilename: production
-      ? "[name].[chunkhash].chunk.js"
-      : "[name].[hash].chunk.js",
+            ? "[name].[chunkhash].chunk.js"
+            : "[name].[hash].chunk.js",
     },
     devServer: {
         contentBase: outDir,
-    // serve index.html for all 404 (required for push-state)
+        // serve index.html for all 404 (required for push-state)
         historyApiFallback: true,
     },
+    devtool: production ? "nosources-source-map" : "cheap-module-eval-source-map",
     module: {
         rules: [
-            ...configCss(extractCss),
-            ...configSass(extractCss),
-      { test: /\.html$/i, loader: "html-loader" },
+            // CSS required in JS/TS files should use the style-loader that
+            // auto-injects it into the website only when the issuer is a .js/.ts file,
+            // so the loaders are not applied inside html templates
+            {
+                test: /\.css$/i,
+                issuer: [{ not: [{ test: /\.html$/i }] }],
+                use: extractCss ? ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: cssRules,
+                }) : ["style-loader", ...cssRules],
+            },
+            {
+                test: /\.css$/i,
+                issuer: [{ test: /\.html$/i }],
+                // CSS required in templates cannot be extracted safely
+                // because Aurelia would try to require it again in runtime
+                use: cssRules,
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+                issuer: /\.[tj]s$/i,
+            },
+            {
+                test: /\.scss$/,
+                use: ["css-loader", "sass-loader"],
+                issuer: /\.html?$/i,
+            },
+            { test: /\.html$/i, loader: "html-loader" },
             {
                 test: /\.js$/i,
                 loader: "babel-loader",
                 exclude: nodeModulesDir,
                 options: coverage ? { sourceMap: "inline", plugins: ["istanbul"] } : {},
             },
-      { test: /\.json$/i, loader: "json-loader" },
-      // use Bluebird as the global Promise implementation:
+            { test: /\.json$/i, loader: "json-loader" },
+            // use Bluebird as the global Promise implementation:
             {
                 test: /[/\\]node_modules[/\\]bluebird[/\\].+\.js$/,
                 loader: "expose-loader?Promise",
             },
-      // exposes jQuery globally as $ and as jQuery:
+            // exposes jQuery globally as $ and as jQuery:
             {
                 test: require.resolve("jquery"),
                 loader: "expose-loader?$!expose-loader?jQuery",
             },
-      // embed small images and fonts as Data Urls and larger ones as files:
+            // embed small images and fonts as Data Urls and larger ones as files:
             {
                 test: /\.(png|gif|jpg|cur)$/i,
                 loader: "url-loader",
@@ -86,7 +122,7 @@ module.exports = ({ production, server, extractCss, coverage } = {}) => ({
                 loader: "url-loader",
                 options: { limit: 10000, mimetype: "application/font-woff" },
             },
-      // load these fonts normally, as files:
+            // load these fonts normally, as files:
             {
                 test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
                 loader: "file-loader",
@@ -102,35 +138,41 @@ module.exports = ({ production, server, extractCss, coverage } = {}) => ({
             "window.jQuery": "jquery",
             Popper: ["popper.js", "default"],
         }),
+        new ModuleDependenciesPlugin({
+            "aurelia-testing": ["./compile-spy", "./view-spy"],
+        }),
         new HtmlWebpackPlugin({
             template: "index.ejs",
             minify: production
-        ? {
-            removeComments: true,
-            collapseWhitespace: true,
-        }
-        : undefined,
+                ? {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                }
+                : undefined,
             metadata: {
-        // available in index.ejs
+                // available in index.ejs
                 title,
                 server,
                 baseUrl,
             },
         }),
+        ...when(
+            extractCss,
+            new ExtractTextPlugin({
+                filename: production ? "[contenthash].css" : "[id].css",
+                allChunks: true,
+            })),
+        ...when(
+            production,
+            new CommonsChunkPlugin({
+                name: "common",
+            })),
         new CopyWebpackPlugin([
-            { from: "favicon.ico", to: "favicon.ico" },
+            { from: "static/favicon.ico", to: "favicon.ico" },
             { from: "config/config.json", to: "config/config.json" },
             { from: "node_modules/font-awesome/fonts", to: "fonts" }]),
-        ...when(
-      extractCss,
-      new ExtractTextPlugin({
-          filename: production ? "[contenthash].css" : "[id].css",
-          allChunks: true,
-      })),
-        ...when(
-      production,
-      new CommonsChunkPlugin({
-          name: "common",
-      })),
+        ...when(production, new UglifyJsPlugin({
+            sourceMap: true,
+        })),
     ],
 })
