0715d678e2db7a51fd6b9b7b29e349230d90d77d
Harish Babu <harish_babu@outlook.com>
2017-12-11 20:29:54 +0000

    fixed eslint error in aurelia_project generators

--- aurelia_project/generators/generator.js
+++ aurelia_project/generators/generator.js
@@ -1,32 +1,32 @@
-import {inject} from 'aurelia-dependency-injection';
-import {Project, ProjectItem, CLIOptions, UI} from 'aurelia-cli';
+import { inject } from "aurelia-dependency-injection"
+import { Project, ProjectItem, CLIOptions, UI } from "aurelia-cli"
 
 @inject(Project, CLIOptions, UI)
 export default class GeneratorGenerator {
-  constructor(project, options, ui) {
-    this.project = project;
-    this.options = options;
-    this.ui = ui;
-  }
+    constructor(project, options, ui) {
+        this.project = project
+        this.options = options
+        this.ui = ui
+    }
 
-  execute() {
-    return this.ui
-      .ensureAnswer(this.options.args[0], 'What would you like to call the generator?')
-      .then(name => {
-        let fileName = this.project.makeFileName(name);
-        let className = this.project.makeClassName(name);
+    execute() {
+        return this.ui
+      .ensureAnswer(this.options.args[0], "What would you like to call the generator?")
+      .then((name) => {
+          const fileName = this.project.makeFileName(name)
+          const className = this.project.makeClassName(name)
 
-        this.project.generators.add(
-          ProjectItem.text(`${fileName}.js`, this.generateSource(className))
-        );
+          this.project.generators.add(
+          ProjectItem.text(`${fileName}.js`, this.generateSource(className)),
+        )
 
-        return this.project.commitChanges()
-          .then(() => this.ui.log(`Created ${fileName}.`));
-      });
-  }
+          return this.project.commitChanges()
+          .then(() => this.ui.log(`Created ${fileName}.`))
+      })
+    }
 
-  generateSource(className) {
-    return `import {inject} from 'aurelia-dependency-injection';
+    generateSource(className) {
+        return `import {inject} from 'aurelia-dependency-injection';
 import {Project, ProjectItem, CLIOptions, UI} from 'aurelia-cli';
 
 @inject(Project, CLIOptions, UI)
@@ -68,6 +68,6 @@ export class \${className} {
   }
 }
 
-`;
-  }
+`
+    }
 }
