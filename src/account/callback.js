4d674dec42bac0c3d2a19715918b3a65e95f5694
Harish Babu <harish_babu@outlook.com>
2021-04-03 23:14:15 +0100

    🚨 introduce npm lint script
    ⬆️ package upgrade eslint and babel-eslint packages

--- src/account/callback.js
+++ src/account/callback.js
@@ -1,31 +1,36 @@
-import { inject } from "aurelia-framework"
-import { AuthService } from "../services/auth-service"
+import { inject } from "aurelia-framework";
+import { AuthService } from "../services/auth-service";
 
 @inject(AuthService)
 export class SignInCallbackViewModel {
     constructor(authService) {
-        let qs = SignInCallbackViewModel.parseQueryString(window.location.search.substr(1))
+        let qs = SignInCallbackViewModel.parseQueryString(
+            window.location.search.substr(1)
+        );
         if (qs.action && qs.action === "logout") {
-            authService.logout()
+            authService.logout();
         } else {
-            qs = SignInCallbackViewModel.parseQueryString(window.location.hash.substr(1))
-            authService.login(qs)
+            qs = SignInCallbackViewModel.parseQueryString(
+                window.location.hash.substr(1)
+            );
+            authService.login(qs);
         }
     }
 
     static parseQueryString(value) {
-        return value.split("&")
+        return value
+            .split("&")
             .map((r) => {
-                const pair = r.split("=")
+                const pair = r.split("=");
                 return {
                     [pair[0]]: pair[1],
-                }
+                };
             })
             .reduce((result, item) => {
-                const key = Object.keys(item)[0]
+                const key = Object.keys(item)[0];
                 return Object.assign({}, result, {
                     [key]: item[key],
-                })
-            }, {})
+                });
+            }, {});
     }
 }
