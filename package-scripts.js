276e070df7b3d37bed28160112e50344fdab0733
Harish Babu <harish_babu@outlook.com>
2021-04-04 12:44:27 +0100

    npm package upgrades (#44)
    * :arrow_up: update whatwg-fetch package
    
    * :arrow_up: upgrade package isomorphic-fetch
    
    * 🚨 introduce npm lint script
    
    ⬆️ package upgrade eslint and babel-eslint packages
    
    * 🚨 htmlhint lint script added fix fixes
    
    * aurelia minor version package updates
    
    * ⬆️ more package upgrades

--- package-scripts.js
+++ package-scripts.js
@@ -1,4 +1,4 @@
-const { series, crossEnv, rimraf } = require("nps-utils") // eslint-disable-line import/no-extraneous-dependencies
+const { series, crossEnv, rimraf } = require("nps-utils"); // eslint-disable-line import/no-extraneous-dependencies
 
 module.exports = {
     scripts: {
@@ -8,8 +8,10 @@ module.exports = {
             karma: {
                 default: series(
                     rimraf("test/coverage-karma"),
-                    "karma start test/karma.conf.js"),
-                debug: "karma start test/karma.conf.js --auto-watch --no-single-run --debug",
+                    "karma start test/karma.conf.js"
+                ),
+                debug:
+                    "karma start test/karma.conf.js --auto-watch --no-single-run --debug",
             },
         },
         build: "nps webpack.build",
@@ -21,15 +23,17 @@ module.exports = {
                 development: {
                     default: series(
                         "nps webpack.build.before",
-                        "webpack --progress -d"),
-                    serve: series.nps(
-                        "webpack.build.development",
-                        "serve"),
+                        "webpack --progress -d"
+                    ),
+                    serve: series.nps("webpack.build.development", "serve"),
                 },
                 production: {
                     default: series(
                         "nps webpack.build.before",
-                        crossEnv("NODE_ENV=production webpack --progress -p --env.production --env.extractCss")),
+                        crossEnv(
+                            "NODE_ENV=production webpack --progress -p --env.production --env.extractCss"
+                        )
+                    ),
                 },
             },
             server: {
@@ -42,4 +46,4 @@ module.exports = {
         },
         serve: "pushstate-server dist",
     },
-}
+};
