07f6218bca46963756a8cafd2390d3132e68a752
Harish Babu <harish_babu@outlook.com>
2021-04-04 18:05:21 +0100

    📦 webpack packages upgraded, with webpack config

--- src/invoices/detail.spec.js
+++ src/invoices/detail.spec.js
@@ -1,12 +1,11 @@
-import sinon from "sinon"
 import { ApiService } from "../services/api-service"
 import { InvoiceDetailViewModel } from "./detail"
 
 describe("InvoiceDetailViewModel", () => {
     describe("activate", () => {
         it("sets the invoice field to return from invoice svc", async () => {
-            const orgService = sinon.createStubInstance(ApiService)
-            const invoiceService = sinon.createStubInstance(ApiService)
+            const orgService = { getById: jest.fn() }
+            const invoiceService = { getById: jest.fn() }
             const serviceFactory = (resource) => {
                 if (resource === "invoice") {
                     return invoiceService
@@ -24,13 +23,14 @@ describe("InvoiceDetailViewModel", () => {
                 items: [],
             }
             const params = { id: 1 }
-            invoiceService.getById.withArgs(params.id)
-                .resolves(invoice)
-            orgService.getById.withArgs(invoice.organization.id)
-                .resolves(invoice.organization)
+            invoiceService.getById.mockResolvedValue(invoice);
+            orgService.getById.mockResolvedValue(invoice.organization);
+            
             const sut = new InvoiceDetailViewModel({}, serviceFactory)
             await sut.activate(params)
             expect(sut.invoice).toBe(invoice)
+            expect(invoiceService.getById).toHaveBeenCalledWith(params.id);
+            expect(orgService.getById).toHaveBeenCalledWith(invoice.organization.id);
         })
     })
 })
